aed2bbd51d064cad37b73091224b199c25a795a8
SantinoPoli <47x9qyygc2@privaterelay.appleid.com>
2026-01-27 03:48:53 +0100

    fix footer bug

--- STCMS_app/src/app/shared/components/footer/footer.ts
+++ STCMS_app/src/app/shared/components/footer/footer.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './footer.html',
-  styleUrl: './footer.css'
+  styleUrl: './footer.css',
 })
-export class Footer {
-
-}
+export class Footer {}
